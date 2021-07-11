debb9fe84008e54295341a483501113bf9c94b47
Raaj Kanchan <iamraajkanchan@gmail.com>
2021-07-11 19:08:10 +0530

    MainActivity updated in Suspended-Function

--- app/src/main/java/com/example/coroutinesexample/MainActivity.kt
+++ app/src/main/java/com/example/coroutinesexample/MainActivity.kt
@@ -16,6 +16,23 @@ class MainActivity : AppCompatActivity()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        
+        GlobalScope.launch {
+            val networkCallResponse1 = doNetworkCalls1()
+            Log.d(TAG , "Network Call Response 1: $networkCallResponse1")
+            val networkCallResponse2 = doNetworkCalls2()
+            Log.d(TAG , "Network Call Response 2: $networkCallResponse2")
+        }
+    }
+
+    suspend fun doNetworkCalls1() : String
+    {
+        delay(3000L)
+        return "This is the response from first resource"
+    }
+
+    suspend fun doNetworkCalls2() : String
+    {
+        delay(3000L)
+        return "This is the response from 2nd resource"
     }
 }
